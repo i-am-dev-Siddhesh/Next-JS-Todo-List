891b0c7a610f939e3ae875e4ba76a617d82617df
i-am-dev-Siddhesh <shindesiddhesh.co.in@gmail.com>
2022-02-03 22:39:34 +0530

    feat: login route added

--- components/TodoList.js
+++ components/TodoList.js
@@ -1,40 +1,53 @@
-import { useState, useEffect } from 'react'
-import { supabase } from '../lib/initSupabase'
+import { useState, useEffect } from "react";
+import { supabase } from "../lib/initSupabase";
 
 export default function Todos({ user }) {
-  const [todos, setTodos] = useState([])
-  const [newTaskText, setNewTaskText] = useState('')
-  const [errorText, setError] = useState('')
+  const [todos, setTodos] = useState([]);
+  const [newTaskText, setNewTaskText] = useState("");
+  const [errorText, setError] = useState("");
 
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
+    setIsLoading(true);
+
     fetchTodos()
-  }, [])
+      .then((resp) => setTodos(resp))
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
 
   const fetchTodos = async () => {
-    let { data: todos, error } = await supabase.from('todos').select('*').order('id', true)
-    if (error) console.log('error', error)
-    else setTodos(todos)
-  }
+    let { data: todos, error } = await supabase
+      .from("todos")
+      .select("*")
+      .eq("user_id", user?.id)
+      .order("id", true);
+
+    if (error) setError(error?.message);
+    else return todos;
+  };
+
   const addTodo = async (taskText) => {
-    let task = taskText.trim()
+    let task = taskText.trim();
     if (task.length) {
       let { data: todo, error } = await supabase
-        .from('todos')
-        .insert({ task, user_id: user.id })
-        .single()
-      if (error) setError(error.message)
-      else setTodos([...todos, todo])
+        .from("todos")
+        .insert({ task, user_id: user?.id })
+        .single();
+      if (error) setError(error?.message);
+      else setTodos([...todos, todo]);
     }
-  }
+  };
 
   const deleteTodo = async (id) => {
     try {
-      await supabase.from('todos').delete().eq('id', id)
-      setTodos(todos.filter((x) => x.id != id))
+      await supabase.from("todos").delete().eq("id", id);
+      setTodos(todos.filter((x) => x.id != id));
     } catch (error) {
-      console.log('error', error)
+      setError(error?.message);
     }
-  }
+  };
 
   return (
     <div className="w-full">
@@ -46,8 +59,8 @@ export default function Todos({ user }) {
           placeholder="make coffee"
           value={newTaskText}
           onChange={(e) => {
-            setError('')
-            setNewTaskText(e.target.value)
+            setError("");
+            setNewTaskText(e.target.value);
           }}
         />
         <button className="btn-black" onClick={() => addTodo(newTaskText)}>
@@ -55,65 +68,76 @@ export default function Todos({ user }) {
         </button>
       </div>
       {!!errorText && <Alert text={errorText} />}
+      {isLoading ? "Loading" : null}
       <div className="bg-white shadow overflow-hidden rounded-md">
         <ul>
-          {todos.map((todo) => (
-            <Todo key={todo.id} todo={todo} onDelete={() => deleteTodo(todo.id)} />
+          {todos?.map((todo) => (
+            <Todo
+              key={todo?.id}
+              todo={todo}
+              onDelete={() => deleteTodo(todo?.id)}
+            />
           ))}
         </ul>
       </div>
     </div>
-  )
+  );
 }
 
 const Todo = ({ todo, onDelete }) => {
-  const [isCompleted, setIsCompleted] = useState(todo.is_complete)
+  const [isCompleted, setIsCompleted] = useState(todo.is_complete);
 
   const toggle = async () => {
     try {
       const { data, error } = await supabase
-        .from('todos')
+        .from("todos")
         .update({ is_complete: !isCompleted })
-        .eq('id', todo.id)
-        .single()
+        .eq("id", todo.id)
+        .single();
       if (error) {
-        throw new Error(error)
+        throw new Error(error);
       }
-      setIsCompleted(data.is_complete)
+      setIsCompleted(data.is_complete);
     } catch (error) {
-      console.log('error', error)
+      console.log("error", error);
     }
-  }
+  };
 
   return (
     <li
       onClick={(e) => {
-        e.preventDefault()
-        toggle()
+        e.preventDefault();
+        toggle();
       }}
       className="w-full block cursor-pointer hover:bg-gray-200 focus:outline-none focus:bg-gray-200 transition duration-150 ease-in-out"
     >
       <div className="flex items-center px-4 py-4 sm:px-6">
         <div className="min-w-0 flex-1 flex items-center">
-          <div className="text-sm leading-5 font-medium truncate">{todo.task}</div>
+          <div className="text-sm leading-5 font-medium truncate">
+            {todo.task}
+          </div>
         </div>
         <div>
           <input
             className="cursor-pointer"
             onChange={(e) => toggle()}
             type="checkbox"
-            checked={isCompleted ? true : ''}
+            checked={isCompleted ? true : ""}
           />
         </div>
         <button
           onClick={(e) => {
-            e.preventDefault()
-            e.stopPropagation()
-            onDelete()
+            e.preventDefault();
+            e.stopPropagation();
+            onDelete();
           }}
           className="w-4 h-4 ml-2 border-2 hover:border-black rounded"
         >
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="gray">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="gray"
+          >
             <path
               fillRule="evenodd"
               d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
@@ -123,11 +147,11 @@ const Todo = ({ todo, onDelete }) => {
         </button>
       </div>
     </li>
-  )
-}
+  );
+};
 
 const Alert = ({ text }) => (
   <div className="rounded-md bg-red-100 p-4 my-3">
     <div className="text-sm leading-5 text-red-700">{text}</div>
   </div>
-)
+);
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,30 @@
-import { supabase } from '../lib/initSupabase'
-import { Auth } from '@supabase/ui'
-import TodoList from '../components/TodoList'
+import { supabase } from "../lib/initSupabase";
+import { Auth } from "@supabase/ui";
+import TodoList from "../components/TodoList";
+import { useEffect, useState } from "react";
 
 export default function IndexPage() {
-  const { user } = Auth.useUser()
+  const [isLoading, setIsLoading] = useState(true);
+  const [user, setUser] = useState();
 
+  const userInfo = Auth.useUser();
+  useEffect(() => {
+    if (userInfo?.user) {
+      setUser(userInfo.user);
+    }
+    setIsLoading(false);
+  }, [userInfo]);
+
+  if (isLoading) {
+    return "isLaoding";
+  }
   return (
     <div className="w-full h-full bg-gray-300">
       {!user ? (
-        <div className="w-full h-full flex justify-center items-center p-4">
+        <div className="w-80 h-full flex justify-center items-center p-4">
           <Auth
             supabaseClient={supabase}
-            providers={['google', 'github']}
+            providers={["google", "github"]}
             socialLayout="horizontal"
             socialButtonSize="xlarge"
           />
@@ -19,14 +32,14 @@ export default function IndexPage() {
       ) : (
         <div
           className="w-full h-full flex flex-col justify-center items-center p-4"
-          style={{ minWidth: 250, maxWidth: 600, margin: 'auto' }}
+          style={{ minWidth: 250, maxWidth: 600, margin: "auto" }}
         >
           <TodoList user={supabase.auth.user()} />
           <button
             className="btn-black w-full mt-12"
             onClick={async () => {
-              const { error } = await supabase.auth.signOut()
-              if (error) console.log('Error logging out:', error.message)
+              const { error } = await supabase.auth.signOut();
+              if (error) console.log("Error logging out:", error.message);
             }}
           >
             Logout
@@ -34,5 +47,5 @@ export default function IndexPage() {
         </div>
       )}
     </div>
-  )
+  );
 }
